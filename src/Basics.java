c70f94c8aedb79edeac092a10465f6ef7af12de7
redreoicy <40970533+redreoicy@users.noreply.github.com>
2019-11-14 11:31:59 -0500

    Update Basics.java

--- src/Basics.java
+++ src/Basics.java
@@ -1,9 +1,34 @@
-
+import java.util.Scanner;
+import java.util.Random;
 public class Basics {
 
 	public static void main(String[] args) {
+		//Mini practice one by one 
 		System.out.println("Hello, World!");
 
+		
+		
+		//number guessing
+		Scanner stdin = new Scanner(System.in);
+		Random rand = new Random();
+		System.out.println("I'm thinking of an integer from 1-10000. Guess it!");
+		
+		int randomInt = rand.nextInt(9999) + 1;
+		int guess = -1;
+		while(guess!=randomInt) {
+			guess = stdin.nextInt();
+			if (guess > randomInt)
+				System.out.println("Too High!");
+			else if (guess < randomInt)
+				System.out.println("Too Low!");
+			else 
+				System.out.println("Correct!");
+		}
+		stdin.close();
+		
+		
 	}
+	
+	
 
 }
